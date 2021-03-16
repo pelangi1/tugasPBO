c47ad8767d34d9ffba138754a22721bde8042200
keen eyes <56186677+pelangi1@users.noreply.github.com>
2021-03-17 06:20:26 +0700

    versi simple

---
+++ galon2.java
@@ -0,0 +1,18 @@
+package com.mygalon.akuah;
+
+public class Galonn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int gallons;		
+		double liters;		
+
+		gallons = 2;
+
+		liters = gallons *3.7854; 
+		
+		
+		System.out.println(gallons + " gallons is " + liters + " liters.");
+	}
+
+}
